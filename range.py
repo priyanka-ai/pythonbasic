b729c3ef20d2e771cf89e1054a894f9a004b71f1
priyasomanigit <priyankasomani748@gmail.com>
2020-06-15 16:01:30 +0530

    ADDED:program to demostrate of range function

---
+++ range.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 15 15:54:19 2020
+
+@author: priya
+"""
+
+#for x in range(10):
+#    print(x)
+
+#for x in range(1,10):
+ #   print(x)
+#for x in range(1,10,2):
+ #   print(x)
+     
+for x in range(10,1,-1):
+    print(x)
