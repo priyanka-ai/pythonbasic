0840e16d24fce71afe226b7bbd1ba10e37178e5a
priyasomanigit <priyankasomani748@gmail.com>
2020-06-15 16:37:18 +0530

    ADDED:program of pattern

---
+++ pattern4.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 15 16:34:57 2020
+
+@author: priya
+"""
+
+n=int(input("enter number of rows"))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(j,end="")
+    print()
