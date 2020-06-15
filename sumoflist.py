667e75507089704a517ee2cb8c3e69c32f32b01b
priyasomanigit <priyankasomani748@gmail.com>
2020-06-15 16:07:00 +0530

    ADDED:program to calculate sum of list

---
+++ sumoflist.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 15 16:02:25 2020
+
+@author: priya
+"""
+
+list=eval(input("enter some list"))
+sum =0
+for x in list:
+    sum=sum+x
+print("the sum  is",sum)
