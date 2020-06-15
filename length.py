dbf42aeb8cc8adc8d64c8c2cfdb47e8b186f30f3
priyasomanigit <priyankasomani748@gmail.com>
2020-06-15 15:46:56 +0530

    ADDED: program to find length of string without using length function

---
+++ length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 15 15:08:00 2020
+
+@author: priya
+"""
+
+s="priyanka somani"
+count=0
+for x in s:
+    count+=1
+    print(x)
+print("length of string is",count)
