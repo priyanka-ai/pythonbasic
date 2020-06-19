d64518f2c793b12dce2a39349c0fdddd2292c557
priyasomanigit <priyankasomani748@gmail.com>
2020-06-19 19:52:18 +0530

    ADDED:demo code

---
+++ whiledemo1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 19 19:46:12 2020
+
+@author: priya
+"""
+
+n=int(input("enter some number"))
+sum=0
+i=1
+while i<=n:
+    sum+=i
+    i+=1
+print("sum of n number is ",sum)
