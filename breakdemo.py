7399684141bc70ad6e7dc04d927484146d01d1bb
priyasomanigit <priyankasomani748@gmail.com>
2020-06-19 19:59:55 +0530

    ADDED:demo code of break keyword

---
+++ breakdemo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 19 19:56:55 2020
+
+@author: priya
+"""
+
+for i in range(10):
+    if i==7:
+        print("please break")
+        break
+    print(i)
