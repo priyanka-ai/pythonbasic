98d8747571f1100ac0c9fc4eb16b6d6acce24371
priyasomanigit <priyankasomani748@gmail.com>
2020-06-19 20:04:32 +0530

    ADDED:demo code of continue keyword

---
+++ continuedemo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 19 20:00:56 2020
+
+@author: priya
+"""
+
+for i in range(10):
+    if i%2==0:
+        continue
+    print(i)
